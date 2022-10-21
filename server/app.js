7090a710e12b37870caa014dce9f8896e3293d44
Igor <sawqe1@yandex.ru>
2022-10-21 11:43:44 +0300

    send response to client

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import {mainRouter} from './routes/main.routes.js';
 
 export const app = express();
 
-app.use('websocket', );
+app.use('/websocket', mainRouter);
+
+const PORT = 9123;
+app.listen(PORT, '127.0.0.1', () => {
+    console.info(`App has been started on port: ${PORT}`);
+});
--- server/routes/main.routes.js
+++ server/routes/main.routes.js
@@ -1,9 +1,20 @@
 import {Router} from 'express';
+import crypto from "crypto";
 const router = Router();
 
 router.get(
     '/',
     (req, res) => {
-        // TODO
+        res.append('Upgrade', 'websocket');
+        res.append('Connection', 'upgrade');
+
+        const shasum = crypto.createHash('sha1');
+        shasum.update(req.headers['sec-websocket-key'] + '778EAFA5-E914-47DA-95CA-C5AB0DC85B00');
+        const secHeader = shasum.digest('base64');
+        res.append('Sec-WebSocket-Accept', secHeader);
+
+        return res.status(101);
     }
-)
+);
+
+export {router as mainRouter};
